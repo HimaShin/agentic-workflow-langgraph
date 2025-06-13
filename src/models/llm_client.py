88dda5225311959c8d42ce14bca9f7d9166b0bc8
Himakar <131349331+HimaShin@users.noreply.github.com>
2025-06-13 21:27:26 +0530

    fix: simulate LLM output for Streamlit Cloud

--- src/models/llm_client.py
+++ src/models/llm_client.py
@@ -1,9 +1,11 @@
 import os
 
 def ask_llm(prompt):
+    # Simulated response for Streamlit Cloud
     if "STREAMLIT_SERVER_ENABLED" in os.environ:
-        return f"Simulated response from agent for: {prompt}"
+        return f"Simulated response for: {prompt}"
     else:
+        # This works only in local machine with Ollama
         from langchain_community.chat_models import ChatOllama
         llm = ChatOllama(model="gemma:2b")
         return llm.invoke(prompt).content
